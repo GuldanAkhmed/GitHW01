0f7aebce619924a9d04881bf40b3e2e73eacd50f
guldan <gulum1111@gmail.com>
2022-05-15 21:57:41 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    for (int i=1; i<=100; i++){
+    for (int i=1; i<=50; i++){
         System.out.println(i);
     }
     }
